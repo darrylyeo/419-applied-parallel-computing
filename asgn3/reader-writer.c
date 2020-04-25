0b411af723fd8c90d1cfe0118d11b14537c12015
Darryl Yeo <dev@darryl-yeo.com>
2020-04-25 00:18:37 -0700

    Finish implementing asgn3

--- asgn3/reader-writer.c
+++ asgn3/reader-writer.c
@@ -1,37 +1,43 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <pthread.h>
 
+#define INPUT_FILE "reader-writer.c"
+
+#define NUM_THREADS 4
+#define CHUNK_SIZE 100
+
+char *searchStrings[] = {"thread", "reader", "writer", "search"};
+
+int fd;
+char *buffer;
+int bufferSize = 0;
+
+
+pthread_cond_t turn;
+pthread_mutex_t mutex;
 
 int writers;
 int writing;
 int reading;
 
-pthread_cond_t turn;
-pthread_mutex_t mutex;
+void *reader(){
+	int size;
 
-int fd;
-#define BUFFER_SIZE 2000
-char buffer[BUFFER_SIZE];
-
-
-void *reader(char *filename){
 	pthread_mutex_lock(&mutex);
 	while(writers > 0)
 		pthread_cond_wait(&turn, &mutex);
 	reading++;
 	pthread_mutex_unlock(&mutex);
 
-	/* Read stuff */
-	if(fd != -1){
-		fd = open(filename, O_RDONLY, 0640);
-		read(fd, &buffer, BUFFER_SIZE);
-	}else{
-		perror("open");
-		exit(EXIT_FAILURE);
-	}
+	/* Read */
+	lseek(fd, 0, 0);
+	while((size = read(fd, &buffer[bufferSize], CHUNK_SIZE)))
+		bufferSize += size;
+		/*printf("%d\n", *id);*/
 
 	pthread_mutex_lock(&mutex);
 	reading--;
@@ -41,18 +47,45 @@ void *reader(char *filename){
 	return NULL;
 }
 
+void *writer(void *arg){
+	char *searchString = (char *) arg;
+	/*char substring[CHUNK_SIZE];*/
+	int searchLength = strlen(searchString);
+	/*int searchCursor = 0;*/
+	char *searchPointer;
+	char *searchResult;
 
-void *writer(){
 	pthread_mutex_lock(&mutex);
 	writers++;
-	while(reading || writing)
+	while(reading > 0 || writing > 0)
 		pthread_cond_wait(&turn, &mutex);
 	writing++;
 	pthread_mutex_unlock(&mutex);
 
-	/* Write stuff */
-	write(1, &buffer, BUFFER_SIZE);
-	printf("\n");
+	/* Write */
+	for(
+		searchPointer = buffer;
+		searchPointer < buffer + bufferSize && (searchResult = strstr(searchPointer, searchString)) != NULL;
+		searchPointer = searchResult + searchLength
+	){
+		printf("\"%s\" found at index %d\n", searchString, (int) (searchResult - buffer));
+	}
+	/*while(searchCursor < bufferSize){
+		printf("%s %d %d\n", searchString, searchCursor, bufferSize);
+		int size = searchCursor + CHUNK_SIZE < bufferSize
+			? CHUNK_SIZE
+			: bufferSize - CHUNK_SIZE;
+		memcpy(substring, &buffer[searchCursor], size);
+		printf("\nSearching: %s\n%s\n", searchString, substring);
+
+		if((searchPointer = strstr(searchString, substring)) != NULL)
+			printf("\"%s\" found at index %d\n", searchString, (int) searchPointer - searchCursor);
+
+		searchCursor += size - searchLength;
+	}*/
+
+	/*while(writeCursor < bufferSize)
+		write(1, &chunks[writeCursor++], CHUNK_SIZE);*/
 
 	pthread_mutex_lock(&mutex);
 	writing--;
@@ -65,17 +98,28 @@ void *writer(){
 
 
 int main(){
-	pthread_t threads[4];
+	int i;
+	pthread_t readerThreads[NUM_THREADS];
+	pthread_t writerThreads[NUM_THREADS];
 
-	pthread_create(&threads[0], NULL, &reader, "reader-writer.c");
-	pthread_create(&threads[1], NULL, &writer, NULL);
-	pthread_create(&threads[2], NULL, &reader, "reader-writer.c");
-	pthread_create(&threads[3], NULL, &writer, NULL);
+	buffer = calloc(50000, sizeof(char));
 
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
-	pthread_join(threads[2], NULL);
-	pthread_join(threads[3], NULL);
+	if(fd != -1){
+		fd = open(INPUT_FILE, O_RDONLY, 0640);
+	}else{
+		perror("open");
+		return EXIT_FAILURE;
+	}
 
-	return 0;
+	for(i = 0; i < NUM_THREADS; i++)
+		pthread_create(&readerThreads[i], NULL, &reader, &i);
+	for(i = 0; i < NUM_THREADS; i++)
+		pthread_create(&writerThreads[i], NULL, &writer, searchStrings[i]);
+	
+	for(i = 0; i < NUM_THREADS; i++)
+		pthread_join(readerThreads[i], NULL);
+	for(i = 0; i < NUM_THREADS; i++)
+		pthread_join(writerThreads[i], NULL);
+
+	return EXIT_SUCCESS;
 }
