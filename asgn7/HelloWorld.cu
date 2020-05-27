7dc344201b8f1a40fcc281820678d3d6c33e4546
Darryl Yeo <dev@darryl-yeo.com>
2020-05-27 00:16:15 -0700

    Start asgn7

---
+++ asgn7/HelloWorld.cu
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
+
+__global__ void HelloWorld(){
+	printf("Hello World");
+}
+
+int main(void){
+	cudaError_t err = cudaSuccess;
+
+	HelloWorld<<<5,1>>>();
+	HelloWorld<<<5,5>>>();
+	
+	if ((err = cudaGetLastError()) != cudaSuccess){
+		fprintf(stderr, "Failed to launch kernel: %s\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	cudaDeviceSynchronize();
+	
+	return 0;
+}
