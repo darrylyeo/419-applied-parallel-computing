37457b79433eb78c6853c68c7c49e8b07118c113
Darryl Yeo <dev@darryl-yeo.com>
2020-06-11 18:57:08 -0700

    Continue implementing histogram-openmp.c

--- final/histogram-openmp.c
+++ final/histogram-openmp.c
@@ -3,16 +3,18 @@
 #define N 1000
 
 int main(void){
+	int i;
+
 	char buffer[N];
-	for(int i = 0; i < N; i++)
+	for(i = 0; i < N; i++)
 		buffer[i] = 32 + rand() % 95;
 
 	int frequencies[127];
 	#pragma omp for reduce(frequencies) local(i)
-	for(int i = 0; i < N; i++)
+	for(i = 0; i < N; i++)
 		frequencies[buffer[i]]++;
 
-	for(int i = 32; i < 127; i++)
+	for(i = 32; i < 127; i++)
 		printf("%c: %d\n", i, frequencies[i]);
 
 	return 0;
