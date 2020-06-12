0471efb3c6c9ef17b37f116a8888ce859dc7cf3d
Darryl Yeo <dev@darryl-yeo.com>
2020-06-11 18:43:39 -0700

    Start implementing histopgram-cuda.cu

---
+++ final/histogram-cuda.cu
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+// #include <helper_cuda.h>
+
+#define N 20
+#define THREADS_PER_BLOCK 3
+
+__global__ void histogram(char *buffer, char *frequencies){
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if(i < N)
+		frequencies[char[i]]++;
+}
+
+int main(void){
+	cudaError_t err = cudaSuccess;
+
+	size_t size = N * sizeof(int);
+	char *buffer;
+	cudaMallocManaged(&buffer, size); // Use `buffer` on the CPU and/or on any GPU in the accelerated system.
+
+	int *frequencies;
+	cudaMallocManaged(&frequencies, 90);
+
+	for(int i = 0; i < N; i++)
+		buffer[i] = 32 + rand() % 58;
+
+	histogram<<<(N + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(buffer, frequencies);
+
+	if ((err = cudaGetLastError()) != cudaSuccess){
+		fprintf(stderr, "Failed to launch kernel: %s\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	cudaDeviceSynchronize();
+
+	for(int i = 32; i < 90; i++)
+		printf("%c: %d\n", i, frequencies[i]);
+
+	cudaFree(buffer);
+
+	return 0;
+}
